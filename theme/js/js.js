73ef403eaa4d7bcd24716650715f86534cbf7efd
Musaev Ruslan <musya.wow@gmail.com>
2017-05-27 18:29:01 +0300

    slice

--- theme/js/js.js
+++ theme/js/js.js
@@ -1,26 +1,49 @@
 (function ($) {
 
-  if (typeof Drupal != 'undefined') {
-    Drupal.behaviors.wcportalMobileTheme = {
-      attach: function (context, settings) {
-        init();
-      },
-
-      completedCallback: function () {
-        // Do nothing. But it's here in case other modules/themes want to override it.
-      }
-    }
-  }
-
-  $(function () {
-    if (typeof Drupal == 'undefined') {
-      init();
-    }
+  $(window).load(function () {
+    $('.flexslider').flexslider({
+      animation: "slide",
+      controlNav: false,
+      prevText: "",
+      nextText: ""
+    });
   });
 
-  function init() {
+  $(window).load(function () {
+    var $btn = $('.btn-menu');
 
-  }
+    $btn.on('click touch', function (e) {
+      e.preventDefault();
+
+      $('body').toggleClass('mobile-active');
+    })
+  })
+
+  $(window).load(function () {
+    var $elms = $('.el-with-animation');
+    var animationEnd = [];
+
+    $(window).on('resize scroll', checkScroll);
+
+    checkScroll();
+
+    function checkScroll() {
+      if (animationEnd.length === $elms.length) return;
+
+      for (var i = 0; i < $elms.length; i++) {
+        var $currentEl = $elms.eq(i);
+
+        if (!$currentEl.hasClass('animated') && $(window).height() + $(window).scrollTop() > $currentEl.offset().top + $currentEl.height() / 2 + 50) {
+          animate($currentEl);
+        }
+      }
+    }
+
+    function animate(el) {
+      el.addClass('animated');
+      animationEnd.push(1);
+    }
+  });
 
 })(jQuery);
 
