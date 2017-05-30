259c4c24d024d2d9d64299e71e868718ef4da4cf
Musaev Ruslan <musya.wow@gmail.com>
2017-05-30 19:46:45 +0300

    new pages

--- theme/js/js.js
+++ theme/js/js.js
@@ -12,7 +12,7 @@
   });
 
   $(window).load(function () {
-    $('.flexslider .slides').slick({
+    $('.slick-slider .slides').slick({
       infinite: true,
       slidesToShow: 2,
       slidesToScroll: 2,
@@ -51,6 +51,24 @@
       el.addClass('animated');
       animationEnd.push(1);
     }
+  });
+
+  $(window).load(function() {
+    $('.slider-for').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false,
+      fade: true,
+      asNavFor: '.slider-nav'
+    });
+    $('.slider-nav').slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      asNavFor: '.slider-for',
+      dots: true,
+      centerMode: true,
+      focusOnSelect: true
+    });
   });
 
 })(jQuery);
